b9b0866d9758c65ad893631f1c75db7cbc1616b3
Camille Dev <fezandellecamille@gmail.com>
2022-11-18 15:56:53 +0100

    Création du contenu de la page d'accueil, avec le style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Logements from '../api.json';
 
 const Home = () => {
 	return (
 		<div className="app">
 			<Header />
-			<h1>Home</h1>
+			<main>
+				<section className="home-illustration">
+					<h1 className="home-illustration__desk-title">Chez vous, partout et ailleurs</h1>
+					<h1 className="home-illustration__mobile-title">
+						Chez vous,
+						<br />
+						partout et ailleurs
+					</h1>
+				</section>
+				<section className="home-gallery">
+					{Logements.map((logement) => (
+						<Link to={`/logement/${logement.id}`} className="home-card" key={logement.id}>
+							<img src={logement.cover} alt={`${logement.title}`} className="home-card__img" />
+							<h2 className="home-card__title">{logement.title}</h2>
+						</Link>
+					))}
+				</section>
+			</main>
 			<Footer />
 		</div>
 	);
