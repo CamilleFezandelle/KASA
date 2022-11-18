7306f4fc6fdcbeceac30f098458a85d6b72ce4de
Camille Dev <fezandellecamille@gmail.com>
2022-11-18 15:59:45 +0100

    Création du contenu de la page 'Error', avec le style

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,7 +7,20 @@ const Error = () => {
 	return (
 		<div className="app">
 			<Header />
-			<h1>Error</h1>
+			<main>
+				<section className="errorpage-error">
+					<h1 className="errorpage-error__title">404</h1>
+					<h2 className="errorpage-error__desk-subtitle">Oups! La page que vous demandez n'existe pas.</h2>
+					<h2 className="errorpage-error__mobile-subtitle">
+						Oups! La page que
+						<br />
+						vous demandez n'existe pas.
+					</h2>
+					<Link to="/home">
+						<p className="errorpage-error__returnhome">Retourner sur la page d'accueil</p>
+					</Link>
+				</section>
+			</main>
 			<Footer />
 		</div>
 	);
