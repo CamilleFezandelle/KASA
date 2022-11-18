2867ecb29349282ebdd04f156262536d329deb5f
Camille Dev <fezandellecamille@gmail.com>
2022-11-18 15:41:34 +0100

    Mise en place du routage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Logement from './pages/Logement';
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Home />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/a-propos" element={<About />} />
+				<Route path="/logement/:id" element={<Logement />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/Logement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logement = () => {
+	return (
+		<div className="app">
+			<h1>Logement</h1>
+		</div>
+	);
+};
+
+export default Logement;
